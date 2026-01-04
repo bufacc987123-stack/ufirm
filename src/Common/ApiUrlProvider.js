2cc68696602dc63a0c9ad3b49fbc0daac820cb3d
syed aadam ahmad <98835147+syedaadamahmad@users.noreply.github.com>
2026-01-04 21:12:17 +0530

    Initial commit

---
+++ src/Common/ApiUrlProvider.js
@@ -0,0 +1,19 @@
+/*
+Created By:Sanjay Vishwakarma
+Date:Dec 28 2018
+Decription: collection of web api urls
+*/
+
+class APIUrlProvider {
+    constructor() {
+
+
+        this.MainUrl = "https://admin-api.urest.in/api/"
+        // this.MainUrl = "https://localhost:62058/api/"
+
+        //this.complaintUrl  = "https://urest.in/complaint/"
+         this.complaintUrl  = "https://api.urest.in:8096/"
+        // this.complaintUrl  = "http://localhost:62929/"
+    }
+}
+export default APIUrlProvider;
